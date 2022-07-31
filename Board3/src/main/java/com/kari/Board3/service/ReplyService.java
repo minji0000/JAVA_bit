81ea37035f478e69647534a6abab68a8bd48216d
minji0000 <101570773+minji0000@users.noreply.github.com>
2022-07-31 19:07:11 +0900

    2022 07 31 Controller, Service의 주석 추가

--- Board3/src/main/java/com/kari/Board3/service/ReplyService.java
+++ Board3/src/main/java/com/kari/Board3/service/ReplyService.java
@@ -17,11 +17,19 @@ public class ReplyService {
     @Autowired
     SqlSession sqlSession;
 
+    // 파라미터로 들어온 reply r 객체를
+    // 데이터베이스에 등록하는 코드이다.
+    // 즉 댓글을 등록하는 코드
     public void insert(ReplyDTO r) {
         sqlSession.insert(NAMESPACE + ".insert", r);
     }
 
+    // 파라미터로 들어온 int 타입의 boardId를
+    // replyMapper의 selectAll()로 리턴해
+    // 게시물을 보여주는 코드이다.
     public List<ReplyDTO> selectAll(int boardId) {
+        // boardId가 파라미터로 들어오는 이유:
+        // 게시물에 달린 해당 댓글을 보여주어야 하기 때문
         return sqlSession.selectList(NAMESPACE + ".selectAll", boardId);
     }
 }
